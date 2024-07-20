f3253da0fae4e205fc3ae342918486c75a7c075d
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-20 18:40:41 -0400

    Agregar archivos de proyecto.

---
+++ SRP/EnviarMensaje.cs
@@ -0,0 +1,10 @@
+﻿namespace SRP
+{
+    public class EnviarMensaje
+    {
+        public void EnviarCorreoElectronico(string mensaje)
+        {
+            // Logica para enviar correo electronico
+        }
+    }
+}
---
+++ SRP/Persona.cs
@@ -0,0 +1,19 @@
+﻿
+namespace SRP
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string direccion;
+        private string correoElectronico;
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Edad: {edad}");
+            Console.WriteLine($"Direccion: {direccion}");
+            Console.WriteLine($"coreo electronico: {correoElectronico}");
+        }
+    }
+}
